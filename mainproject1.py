e422d1e99383946972d2d743461fd520fad35886
syedasheesh <syedabupower@gmail.com>
2025-10-06 16:45:40 +0530

    Update mainproject1.py

--- mainproject1.py
+++ mainproject1.py
@@ -1,1 +1,7 @@
-print("Hello World")
+def replace_word():
+  string = "Hi Guys, Im syed a new teacher for python"
+  word_to_replace = input('Enter the word to replace : ')
+  word_replacement = input("Enter the word replacement: ")
+  print(string.replace(word_to_replace,word_replacement))
+
+replace_word()

3746b654a70d1a38baffd84e43deb8e2f7d562e1
syedasheesh <syedabupower@gmail.com>
2025-10-07 23:48:42 +0530

    Create project_binary_search.py

---
+++ project_binary_search.py
@@ -0,0 +1,32 @@
+# function that takes a list and target parameter 
+# multiple vaiables : middle, start, end, steps 
+# recusion or while loop 
+# increase the steps each time a spilt is done 
+# conditions to track target position 
+
+def binary_search(list, element):
+    middle = 0
+    start = 0 
+    end = len(list)
+    steps = 0 
+    while (start <= end):
+        print('step',steps,':',list[start:end+1])
+        
+        steps = steps+1 
+        middle = (start + end) // 2 
+        
+        if element == list[middle]:
+            return middle
+        if element < list[middle]:
+            end = middle - 1 
+        else:
+            start = middle + 1 
+            
+    return -1 
+    
+My_list = [1,2,3,4,5,6,7,8,9]
+target = 2
+
+binary_search(My_list,target)
+
+    
